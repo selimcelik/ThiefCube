c5b4c6a234a0eabc2e6e4ebac695e1436c6c9041
Selim Çelik <selimc1761@gmail.com>
2019-08-30 12:21:01 +0300

    Add files via upload

---
+++ Kodlar/Levelend.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Levelend : MonoBehaviour
+{
+
+    void Start()
+    {
+        if (MoveObject.levelEnd)
+        {
+            gameObject.SetActive(true);
+           
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+      
+            if (col.gameObject.tag == "Player")
+            {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        
+    }
+}
---
+++ Kodlar/navmesh.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class navmesh : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    public float gameoverTime=75;
+    public Text gameOverText;
+    public Text gameOverOldunText;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        gameoverTime -= Time.deltaTime;
+        gameOverText.text = "Time = " + (int)gameoverTime;
+        if (gameoverTime <= 0)
+        {
+            gameoverTime = 0;
+            gameOverOldunText.text = "GAME OVER";
+            Invoke("restart", 2f);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+    void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
